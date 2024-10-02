88bb538b8c5875fce34812326205f2ac11ea55f8
HYJIN\isacc <isacc7224@gmail.com>
2024-10-02 11:33:56 +0900

    no message

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAsefeurMDCm5FaEj7xhAk1w6s9rFJLE34",
+  authDomain: "react-shop-48a4f.firebaseapp.com",
+  projectId: "react-shop-48a4f",
+  storageBucket: "react-shop-48a4f.appspot.com",
+  messagingSenderId: "42427420640",
+  appId: "1:42427420640:web:06dc0d366b23a781852585",
+  measurementId: "G-8EXCB9FV40",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
